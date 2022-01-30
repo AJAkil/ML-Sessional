dca8d1237266bc6c69a39f483ad1aeb9bc751250
akil <cyclosis47@gmail.com>
2022-01-30 18:34:06 +0600

    wrote flattened layer

--- Assignment-3/1605079.py
+++ Assignment-3/1605079.py
@@ -1,7 +1,44 @@
 import numpy as np
 import gzip
 import os
+import pickle
 from sklearn import preprocessing
+
+class Utility:
+
+    @staticmethod
+    def one_hot_encode(y_true):
+        # Define the One-hot Encoder
+        ohe = preprocessing.OneHotEncoder()
+        ohe.fit(y_true)
+        y_true = ohe.transform(y_true).toarray()
+        return y_true
+
+    @staticmethod
+    def zero_pad(tensor, pad_size):
+        """
+        :param tensor: tensor of shape (h, w, num_channel)
+        :return: padded tensor of shape (h + 2 * pad_size, w + 2 * pad_size, num_channel)
+        """
+        return np.pad(tensor, ((pad_size, pad_size), (pad_size, pad_size), (0,0)), mode='constant', constant_values=0)
+
+    @staticmethod
+    def convolve_single_step(Z_prev_windowed, W, b):
+        """
+        :param Z_prev_windowed: window of shape (F, F, num_channel_Z_prev)
+        :param W: kernel/filter/weight of shape (F, F, num_channel_Z_prev)
+        :param b: bias term of shape (1, 1, 1)
+        :return: scaler convolved value
+        """
+        return np.multiply(Z_prev_windowed, W).sum() + float(b)
+
+
+    @staticmethod
+    def get_max_pool_window(Z_prev_windowed):
+        return Z_prev_windowed.max()
+
+    def create_mini_batches(self):
+        pass
 
 class MnistDataLoader:
     def __init__(self, data_folder_path):
@@ -52,3 +89,77 @@ class MnistDataLoader:
         self.data[self.data_list[1]] = Utility.one_hot_encode(self.data[self.data_list[1]])
 
         assert self.data[self.data_list[1]].shape == (60000, 10)
+
+
+class Cifer10DataLoader:
+    def __init__(self, data_path):
+        self.data_path = data_path
+        self.size = 32
+        self.color_channel = 3
+        self.per_batch_data_size = 10000
+        self.data = dict()
+
+    def load_data(self, file_name):
+        with open(os.path.join(self.data_path, file_name), 'rb') as f:
+
+            data_dict=pickle.load(f, encoding='latin1')
+
+            images = data_dict['data']
+            labels = data_dict['labels']
+
+            images = images.reshape(self.per_batch_data_size, self.color_channel, self.size, self.size).transpose(0,2,3,1).astype("float")
+            labels = np.array(labels)
+            print(labels.shape)
+
+            return images, labels
+
+    def concatenate_data(self):
+        X1, Y1 = self.load_data('data_batch_1')
+        X2, Y2 = self.load_data('data_batch_2')
+        X3, Y3 = self.load_data('data_batch_3')
+        X4, Y4 = self.load_data('data_batch_4')
+        X5, Y5 = self.load_data('data_batch_5')
+
+        self.data['train_images'] = np.concatenate(
+            (
+                X1, X2, X3, X4, X5
+            ),
+            axis=0
+        )
+
+        self.data['train_labels'] = np.concatenate(
+            (
+                Y1.reshape(self.per_batch_data_size, 1),
+                Y2.reshape(self.per_batch_data_size, 1),
+                Y3.reshape(self.per_batch_data_size, 1),
+                Y4.reshape(self.per_batch_data_size, 1),
+                Y5.reshape(self.per_batch_data_size, 1)
+            ),
+            axis=0
+        )
+
+        X_test, Y_test = self.load_data('test_batch')
+
+        self.data['test_images'] = X_test
+        self.data['test_labels'] = Y_test.reshape(Y_test.shape[0], 1)
+
+        self.assert_data_shape()
+
+        for key, data in self.data.items():
+            print(f'Shape: {data.shape}')
+
+    def assert_data_shape(self):
+        assert self.data['train_images'].shape == (50000, 32, 32, 3)
+        assert self.data['train_labels'].shape == (50000, 1)
+        assert self.data['test_images'].shape  == (10000, 32, 32, 3)
+        assert self.data['test_labels'].shape  == (10000, 1)
+
+    def preprocess_data(self):
+        self.data['train_images'] /= 255
+        self.data['test_images'] /= 255
+
+        self.data['train_labels'] = Utility.one_hot_encode(self.data['train_labels'])
+
+        assert self.data['train_labels'].shape == (50000, 10)
+
+
